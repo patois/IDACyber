039789d2e216e1168821325c8c6f18f5c9aca02a
Dennis Elser <patois@users.noreply.github.com>
2017-07-28 17:57:29 +0200

    Update NES.py

--- cyber/NES.py
+++ cyber/NES.py
@@ -1,10 +1,12 @@
-from PyQt5.QtGui import QColor
+from PyQt5.QtGui import qRgb
 from idacyber import ColorFilter
 
 class NES(ColorFilter):
     name = "NES"
+    highlight_cursor = True
+    help = None
 
-    def do_filter(self, buf, addr):
+    def render_img(self, buf, addr, mouse_offs):
         #Bit    7  6  5  4  3  2  1  0
         #Data   R  R  R  G  G  G  B  B
         colors = []
@@ -13,7 +15,7 @@ class NES(ColorFilter):
             red = c & 0xE0
             green = (c << 3) & 0xE0
             blue = (c << 6) & 0xC0
-            colors.append(QColor(red, green, blue))
+            colors.append(qRgb(red, green, blue))
         return colors
     
 def FILTER_ENTRY():
