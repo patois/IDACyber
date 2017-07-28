bc1c61dbd71ca37ad0fa772cb40065995b5d4b74
Dennis Elser <patois@users.noreply.github.com>
2017-07-28 18:02:21 +0200

    add new colorfilters
    add crawl.py
    add mountain.py

---
+++ cyber/crawl.py
@@ -0,0 +1,67 @@
+from PyQt5.QtGui import qRgb
+from PyQt5.QtCore import Qt
+from idacyber import ColorFilter
+from ida_bytes import get_item_end, get_item_head
+import ida_idaapi
+from ida_kernwin import asklong
+from ida_funcs import get_func, get_func_name
+from ida_name import get_name
+
+class Crawl(ColorFilter):
+    name = "Crawl"
+    highlight_cursor = True
+    help = 'Highlight functions and items - RMB toggles mode'
+
+    def __init__(self):
+        self.hl_color = 100
+        self.switch = 1
+        self.mode = ["Item length", "Functions"]
+
+    def on_mb_click(self, button, addr, mouse_offs):
+        if button == Qt.MiddleButton:
+            pass
+        elif button == Qt.RightButton:
+            self.switch ^= 1
+            print "Highlighting %s" % self.mode[self.switch]
+
+    def get_tooltip(self, addr, mouse_offs):
+        tooltip = '%X: ' % (addr + mouse_offs)
+
+        if self.switch == 0:
+            tooltip += '%s' % get_name(get_item_head(addr + mouse_offs))
+        else:
+            f = get_func(addr + mouse_offs)
+            if f:
+                tooltip += '%s' % get_func_name(f.startEA)
+        return tooltip
+
+    def render_img(self, buf, addr, mouse_offs):
+        colors = []
+        head = ida_idaapi.BADADDR
+        tail = ida_idaapi.BADADDR
+
+        if mouse_offs is not None:
+            if self.switch == 0: # data
+                head = get_item_head(addr + mouse_offs)
+                tail = get_item_end(addr + mouse_offs)
+            else: # code
+                f = get_func(addr + mouse_offs)
+                if f:
+                    head = f.startEA
+                    tail = f.endEA
+
+        for pos in xrange(len(buf)):
+            c = ord(buf[pos]) & 0xFF
+            
+            highlight = False
+            if mouse_offs is not None:
+                if addr + pos >= head and addr + pos < tail:
+                    highlight = True
+            if highlight:
+                colors.append(qRgb(c, 0xFF, self.hl_color))
+            else:
+                colors.append(qRgb(c, 0, 0))
+        return colors
+
+def FILTER_ENTRY():
+    return Crawl()
---
+++ cyber/mountain.py
@@ -0,0 +1,44 @@
+from PyQt5.QtGui import qRgb
+from idacyber import ColorFilter
+from ida_bytes import get_byte
+from ida_funcs import get_func
+from ida_bytes import get_flags, is_strlit, get_item_head
+
+class Mountain(ColorFilter):
+    name = "Mountain"
+    highlight_cursor = True
+    help = 'Emphasizes functions and strings'
+
+    def on_activate(self, idx):
+        print Mountain.help
+
+    def on_mb_click(self, button, addr, mouse_offs):
+        print 'click at %X' % (addr + mouse_offs)
+
+    def _is_string(self, ea):
+        head = get_item_head(ea)
+        flags = get_flags(head)
+        return is_strlit(flags)
+
+
+    def render_img(self, buf, addr, mouse_offs):
+        colors = []
+        for offs in xrange(len(buf)):
+            r = g = b = 0
+            c = ord(buf[offs]) & 0xFF
+            ea = addr + offs
+            f = get_func(ea)
+            if f:
+                g = b = c
+            elif self._is_string(ea):
+                g = c
+            else:
+                r = g = b = c
+            colors.append(qRgb(r, g, b))
+        return colors
+
+    def get_tooltip(self, addr, mouse_offs):
+        return "0x%02X" % get_byte(addr + mouse_offs)
+    
+def FILTER_ENTRY():
+    return Mountain()
