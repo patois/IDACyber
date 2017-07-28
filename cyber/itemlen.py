77d02061e8355680f2cff87150d2b628f690eafc
Dennis Elser <patois@users.noreply.github.com>
2017-07-28 17:59:33 +0200

    Update itemlen.py

--- cyber/itemlen.py
+++ cyber/itemlen.py
@@ -1,18 +1,20 @@
-from PyQt5.QtGui import QColor
+from PyQt5.QtGui import qRgb
 from idacyber import ColorFilter
 from ida_bytes import get_item_size, get_item_head
 from copy import copy
 
 class ItemLength(ColorFilter):
     name = "Item length"
+    highlight_cursor = True
+    help = None
 
-    def do_filter(self, buf, addr):
+    def render_img(self, buf, addr, mouse_offs):
         colors = []
         start = addr
         end = start + len(buf)
         ea = start
-        col1 = QColor(0xDC, 0xDC, 0xDC)
-        col2 = QColor(0x00, 0x00, 0x00)
+        col1 = qRgb(0xDC, 0xDC, 0xDC)
+        col2 = qRgb(0x00, 0x00, 0x00)
         cols = [col1, col2]
 
         pos = 0
