3eaeec8302c7880c03817aa07194fba59f7fc183
Dennis Elser <patois@users.noreply.github.com>
2017-07-28 18:00:01 +0200

    Update xor.py

--- cyber/xor.py
+++ cyber/xor.py
@@ -1,4 +1,4 @@
-from PyQt5.QtGui import QColor
+from PyQt5.QtGui import qRgb
 from PyQt5.QtCore import Qt
 from idacyber import ColorFilter
 from ida_kernwin import asklong
@@ -6,6 +6,8 @@ from ida_bytes import get_byte
 
 class Xor(ColorFilter):
     name = "XOR"
+    highlight_cursor = True
+    help = None
 
     def __init__(self):
         self.key = 23
@@ -19,22 +21,22 @@ class Xor(ColorFilter):
     def on_activate(self, idx):
         print "%s filter:\n  * pick key from graph with right mouse button\n  * assign key with middle mouse button." % Xor.name
 
-    def on_mb_click(self, addr, button):
+    def on_mb_click(self, button, addr, mouse_offs):
         if button == Qt.MiddleButton:
             self._set_xor_key()
         elif button == Qt.RightButton:
-            key = get_byte(addr)
+            key = get_byte(addr + mouse_offs)
             self._set_xor_key(key)
 
-    def do_filter(self, buf, addr):
+    def render_img(self, buf, addr, mouse_offs):
         colors = []
         for c in buf:
             c = (ord(c) ^ self.key) & 0xFF
-            colors.append(QColor(0, c, c))
+            colors.append(qRgb(0, c, c))
         return colors
 
-    def get_tooltip(self, addr):
-        return "%X:\nCursor 0x%02X\nKey: 0x%02X" % (addr, get_byte(addr), self.key)
+    def get_tooltip(self, addr, mouse_offs):
+        return "%X:\nCursor 0x%02X\nKey: 0x%02X" % (addr + mouse_offs, get_byte(addr + mouse_offs), self.key)
 
 def FILTER_ENTRY():
     return Xor()
