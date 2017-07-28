26e35b4beb17775c99e73255e7b4496f1355a739
Dennis Elser <patois@users.noreply.github.com>
2017-07-28 17:58:27 +0200

    Update expr.py

--- cyber/expr.py
+++ cyber/expr.py
@@ -1,10 +1,12 @@
-from PyQt5.QtGui import QColor
+from PyQt5.QtGui import qRgb
 from PyQt5.QtCore import Qt
 from idacyber import ColorFilter
 from ida_kernwin import askstr, warning
 
 class xpression(ColorFilter):
     name = "expression"
+    highlight_cursor = True
+    help = None
 
     def __init__(self):
         self.xpr = "c, c, c"
@@ -26,16 +28,16 @@ class xpression(ColorFilter):
 
 
 
-    def on_mb_click(self, addr, button):
+    def on_mb_click(self, button, addr, mouse_offs):
         if button == Qt.RightButton:
             self._set_user_expr()
 
-    def do_filter(self, buf, addr):
+    def render_img(self, buf, addr, mouse_offs):
         colors = []
         for c in buf:
             c = ord(c) & 0xFF
             r, g, b = eval(self.xpr)
-            colors.append(QColor(r&0xFF, g&0xFF, b&0xFF))
+            colors.append(qRgb(r&0xFF, g&0xFF, b&0xFF))
         return colors
 
 def FILTER_ENTRY():
